c1fd5b98f1a1a8eb6a4b2c2941db05806cbadffd
grapeit <alexgrapeit@gmail.com>
2019-06-11 21:55:56 -0500

    db schema

--- server/db.sql
+++ server/db.sql
@@ -1,18 +1,37 @@
-CREATE TABLE IF NOT EXISTS `data_log` (
-  bike INT NOT NULL,
-  ts DOUBLE NOT NULL,
-  gear DOUBLE,
-  throttle DOUBLE,
-  rpm DOUBLE,
-  speed DOUBLE,
-  coolant DOUBLE,
-  battery DOUBLE,
-  map DOUBLE,
-  trip DOUBLE,
-  odometer DOUBLE,
-  PRIMARY KEY `pk` (`bike`,`ts`)
-) DEFAULT CHARSET=utf8;
+CREATE DATABASE IF NOT EXISTS `cresson` CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT USAGE ON cresson.* TO 'cresson'@'localhost' IDENTIFIED BY 'blaxamuxanazad';
+GRANT USAGE ON cresson.* TO 'cresson'@'localhost' IDENTIFIED BY 'your-password';
 GRANT ALL PRIVILEGES ON cresson.* TO 'cresson'@'localhost';
 FLUSH PRIVILEGES;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `bikes` (
+  `id` INT AUTO_INCREMENT,
+  `user` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_index` (`user`),
+  FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `data_log` (
+  `bike` INT NOT NULL,
+  `ts` DOUBLE NOT NULL,
+  `gear` DOUBLE,
+  `throttle` DOUBLE,
+  `rpm` DOUBLE,
+  `speed` DOUBLE,
+  `coolant` DOUBLE,
+  `battery` DOUBLE,
+  `map` DOUBLE,
+  `trip` DOUBLE,
+  `odometer` DOUBLE,
+  PRIMARY KEY `pk` (`bike`,`ts`),
+  INDEX `bike_index` (`bike`),
+  FOREIGN KEY (`bike`) REFERENCES `bikes`(`id`) ON DELETE CASCADE,
+  INDEX `ts_index` (`ts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
