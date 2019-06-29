090c5895048b68499b3041e2fc88ca91b085dd2e
grapeit <alexgrapeit@gmail.com>
2019-06-29 12:33:23 -0500

    environment variables

--- server/api/config.go
+++ server/api/config.go
@@ -19,16 +19,16 @@ var config Configuration
 
 
 func initConfig() {
-	port := toInt(os.Getenv("CRESSON_PORT"))
+	port := toInt(os.Getenv("CRESSON_API_PORT"))
 	if port == 0 {
 		port = defaultPort
 	}
 	config.listen = fmt.Sprintf(":%d", port)
-	config.authSecret = os.Getenv("CRESSON_AUTH_SECRET")
-	config.dbDriverName = os.Getenv("CRESSON_DB_DRIVER")
+	config.authSecret = os.Getenv("CRESSON_API_AUTH_SECRET")
+	config.dbDriverName = os.Getenv("CRESSON_API_DB_DRIVER")
 	if config.dbDriverName == "" {
 		config.dbDriverName = defaultDbDriver
 	}
-	config.dbConnectString = os.Getenv("CRESSON_DB_CONNECT")
-	config.debug = toInt(os.Getenv("CRESSON_DEBUG")) != 0
+	config.dbConnectString = os.Getenv("CRESSON_API_DB_CONNECT")
+	config.debug = toInt(os.Getenv("CRESSON_API_DEBUG")) != 0
 }
